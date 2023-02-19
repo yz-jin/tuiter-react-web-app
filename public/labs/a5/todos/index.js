82d1a8a5c1143c968f904587ac0a9a69a65737b9
yzjin117 <yzjin117@uw.edu>
2023-02-18 17:05:25 -0800

    a5 lab

---
+++ public/labs/a5/todos/index.js
@@ -0,0 +1,16 @@
+// alert('Hello World!');
+/* eslint-env jquery */
+import TodoList from "./TodoList.js";
+
+// $('#wd-todo').append(`
+//     <div class="container">
+//         <h1>Todo example</h1>
+//     </div>
+// `);
+
+$('#wd-todo').append(`
+    <div class="container">
+        <h1>Todo example</h1>
+        ${TodoList()}
+    </div>
+`);
