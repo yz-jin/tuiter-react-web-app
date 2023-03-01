e031dc4096b4858a65b79815cee5b82ba30e60a2
yzjin117 <yzjin117@uw.edu>
2023-03-01 15:27:04 -0800

    a7 lab setup

---
+++ src/labs/a7/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Assignment7 = () => {
+    return(
+        <>
+            <h1>Assignment 7</h1>
+        </>
+    );
+};
+export default Assignment7;
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,11 +1,17 @@
-import Assignment6 from "./a6/index";
 import Nav from "../nav";
-
+import Assignment6 from "./a6";
+import Assignment7 from "./a7";
+import {Routes, Route} from "react-router";
 function Labs() {
-    return(
+    return (
         <div>
             <Nav/>
-            <Assignment6/>
+            <Routes>
+                <Route index
+                       element={<Assignment6/>}/>
+                <Route path="a7"
+                       element={<Assignment7/>}/>
+            </Routes>
         </div>
     );
 }
